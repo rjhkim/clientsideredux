9a03221a4ae84c9e96e3044c2b55acab78a0cb36
Ryan Kim <rjhkim@umich.edu>
2024-06-26 18:04:54 +0900

    working on query for index.py

--- insta485/views/index.py
+++ insta485/views/index.py
@@ -11,6 +11,34 @@ import insta485
 @insta485.app.route('/')
 def show_index():
     """Display / route."""
-    context = {}
-    return flask.render_template("index.html", **context)
 
+    # Connect to database
+    connection = insta485.model.get_db()
+
+    # Query database
+    # get all posts from logname, also users that logname follow
+    # get a container of posts, for each posts their comments
+    # For each post:
+    # user table: username, filename
+    # post table: filename, owner created
+    # like table: like count tied to postid as likes
+    # comments table: comments
+    # like or unlike button
+    # comment input button
+    # following
+
+    logname = "awdeorio"
+    cur = connection.execute(
+        "SELECT users.username, users.filename, posts.filename, posts.created, "
+        "COUNT(likes), comments.owner, comments.text, "
+        "FROM users "
+        "INNER JOIN posts"
+        "ON posts.owner=users.username"
+        "WHERE users.username != ?",
+        (logname, )
+    )
+    users = cur.fetchall()
+
+    # Add database info to context
+    context = {"users": users}
+    return flask.render_template("index.html", **context)
