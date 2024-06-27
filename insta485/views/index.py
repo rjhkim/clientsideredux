58aabd63230d398c888b62a43d653282abc8cc60
Ryan Kim <rjhkim@umich.edu>
2024-06-27 15:38:27 +0900

    index.py and working template

--- insta485/views/index.py
+++ insta485/views/index.py
@@ -6,6 +6,8 @@ URLs include:
 """
 import flask
 import insta485
+import arrow
+from flask import send_from_directory
 
 
 @insta485.app.route('/')
@@ -29,16 +31,69 @@ def show_index():
 
     logname = "awdeorio"
     cur = connection.execute(
-        "SELECT users.username, users.filename, posts.filename, posts.created, "
-        "COUNT(likes), comments.owner, comments.text, "
-        "FROM users "
-        "INNER JOIN posts"
-        "ON posts.owner=users.username"
-        "WHERE users.username != ?",
-        (logname, )
+        "SELECT posts.postid, "
+        "posts.filename AS post_filename, "
+        "posts.created AS post_created, "
+        "users.username AS post_owner, "
+        "users.filename AS owner_filename, "
+        "users.fullname AS owner_fullname, "
+        "COALESCE(likes_count.likes_count, 0) AS likes_count, "
+        "comments.commentid, "
+        "comments.owner AS comment_owner, "
+        "comments.text AS comment_text, "
+        "comments.created AS comment_created "
+        "FROM posts "
+        "JOIN users ON posts.owner = users.username "
+        "LEFT JOIN (SELECT postid, COUNT(*) AS likes_count FROM likes GROUP BY postid) likes_count "
+        "ON posts.postid = likes_count.postid "
+        "LEFT JOIN comments ON posts.postid = comments.postid "
+        "LEFT JOIN users AS comment_users ON comments.owner = comment_users.username "
+        "WHERE posts.owner = ? "
+        "OR posts.owner IN (SELECT username2 FROM following WHERE username1 = ?) "
+        "ORDER BY posts.created DESC, posts.postid DESC, comments.created DESC",
+        (logname, logname)
     )
-    users = cur.fetchall()
+
+    results = cur.fetchall()
+    posts_with_comments = {}
+    for row in results:
+        post_id = row['postid']
+
+        if post_id not in posts_with_comments:
+            posts_with_comments[post_id] = {
+                'post_details': {
+                    'postid': row['postid'],
+                    'post_filename': row['post_filename'],
+                    'post_created': arrow.get(row['post_created']).humanize(),
+                    'post_owner': row['post_owner'],
+                    'owner_filename': row['owner_filename'],
+                    'likes': row['likes_count']
+                },
+                'comments': []
+             }
+
+        #adding comments now
+        if (row['comment_owner'] and row['comment_text'] and row['comment_created']):
+            comment_exists = any(
+                comment['owner'] == row['comment_owner'] and comment['text'] == row['comment_text']
+                and comment['created'] == row['comment_created']
+                for comment in posts_with_comments[post_id]['comments']
+            )
+
+            if not comment_exists:
+                comment = {
+                    'owner': row['comment_owner'],
+                    'text': row['comment_text'],
+                    'created': row['comment_created']
+                }
+                posts_with_comments[post_id]['comments'].append(comment)
+
 
     # Add database info to context
-    context = {"users": users}
+    context = {"posts_with_comments": posts_with_comments}
     return flask.render_template("index.html", **context)
+
+
+@insta485.app.route("/uploads/<path:filename>")
+def send_image(filename):
+    return send_from_directory(insta485.app.config['UPLOAD_FOLDER'], filename)
