7ed2697f9520ee1987d0a1cea422098731c841e0
Ryan Kim <rjhkim@umich.edu>
2024-07-07 15:10:48 +0900

    working html of post and css

--- insta485/views/posts.py
+++ insta485/views/posts.py
@@ -10,12 +10,13 @@ def show_post(postid):
     # Connect to database
     connection = insta485.model.get_db()
 
-    logname = "awedrio"
+    logname = "awdeorio"
 
     #get all info about post
     cur = connection.execute(
         "SELECT DISTINCT posts.filename AS post_filename, "
         "posts.created AS post_created, "
+        "users.filename AS owner_filename, "
         "users.username AS post_owner, "
         "COALESCE(likes_count.likes_count, 0) AS likes_count, "
         "CASE WHEN user_likes.postid IS NOT NULL THEN 1 ELSE 0 END AS user_liked, "
@@ -25,25 +26,29 @@ def show_post(postid):
         "comments.created AS comment_created "
         "FROM posts "
         "JOIN users ON posts.owner = users.username "
-        "LEFT JOIN (SELECT postid, COUNT(*) AS likes_count FROM likes GROUP BY postid) likes_count "
+        "LEFT JOIN (SELECT DISTINCT postid, COUNT(*) AS likes_count FROM likes GROUP BY postid) likes_count "
         "ON posts.postid = likes_count.postid "
-        "LEFT JOIN (SELECT postid FROM likes) user_likes "
+        "LEFT JOIN (SELECT DISTINCT postid FROM likes) user_likes "
         "ON posts.postid = user_likes.postid "
         "LEFT JOIN comments ON posts.postid = comments.postid "
         "LEFT JOIN users AS comment_users ON comments.owner = comment_users.username "
-        "WHERE posts.postid = ?",
+        "WHERE posts.postid = ? "
+        "ORDER BY comments.created DESC",
         (postid,)
     )
 
     results = cur.fetchall()
 
+    print(results)
+
     post_with_comments = {
         'post_details': {
             'post_filename': results[0]['post_filename'],
-            'post_created': results[0]['post_created'],
+            'post_created': arrow.get(results[0]['post_created']).humanize(),
             'post_owner': results[0]['post_owner'],
             'likes_count': results[0]['likes_count'],
-            'user_liked': results[0]['user_liked']
+            'user_liked': results[0]['user_liked'],
+            'owner_filename': results[0]['owner_filename']
         },
         'comments': []
     }
@@ -64,8 +69,10 @@ def show_post(postid):
                 }
                 post_with_comments['comments'].append(comment)
 
-    print(post_with_comments)
 
     # Add database info to context
-    context = {"post_with_comments": post_with_comments}
+    context = {
+        "post_with_comments": post_with_comments,
+        "logname": logname
+        }
     return flask.render_template("posts.html", **context)
